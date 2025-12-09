f4610c41b200c9f9cdf6536a4e7394f0b6cc4633
Kembarius <lukakucic33@gmail.com>
2025-12-10 00:37:18 +0100

    Clean up the conditional rendering of login popup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
 
   return (
     <>
-      {showLogin? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
       <div className='app'>
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
